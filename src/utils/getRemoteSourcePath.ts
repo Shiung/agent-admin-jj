69764f1a4691ea7d75b19eb2c441f1bf514b583b
Jerry <jerry.chou8755@gmail.com>
2025-11-19 12:28:03 +0800

    chore: test env

--- src/utils/getRemoteSourcePath.ts
+++ src/utils/getRemoteSourcePath.ts
@@ -1,4 +1,5 @@
 const remoteImgUrl = import.meta.env.VITE_IMG_URL || ''
+console.log('meta env', import.meta.env)
 
 export default function getRemoteSourcePath(path: string) {
   return `${remoteImgUrl}${path}`
