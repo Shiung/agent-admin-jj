7e97b01d8c1e85d15eefe520534bc7d659d58758
Jerry <jerry.chou8755@gmail.com>
2025-11-19 13:14:06 +0800

    bump 2.0.2

--- src/utils/getRemoteSourcePath.ts
+++ src/utils/getRemoteSourcePath.ts
@@ -1,5 +1,4 @@
 const remoteImgUrl = import.meta.env.VITE_IMG_URL || ''
-console.log('meta env', import.meta.env)
 
 export default function getRemoteSourcePath(path: string) {
   return `${remoteImgUrl}${path}`
