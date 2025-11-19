68819217e0d5445413a3cd3a01758b43833e2140
Jerry <jerry.chou8755@gmail.com>
2025-11-19 12:27:11 +0800

    feat: 增加 splash loading 避免白屏

--- src/main.ts
+++ src/main.ts
@@ -22,5 +22,22 @@ app.use(router)
 
 // 註冊全局指令
 app.directive('click-outside', clickOutside)
+;(async () => {
+  await router.isReady()
 
-app.mount('#app')
+  app.mount('#app')
+
+  // 移除加載動畫
+  const entryLoading = document.getElementById('entry-loading')
+  if (!entryLoading) return
+
+  // 淡出動畫
+  entryLoading.classList.add('hide')
+  entryLoading.addEventListener(
+    'transitionend',
+    () => {
+      entryLoading.remove()
+    },
+    { once: true },
+  )
+})()
