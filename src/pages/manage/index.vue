dc442b472153f22dcbfe29f9154a052622a2e000
ian <ptp_ian@photons.com.tw>
2025-12-12 15:38:16 +0800

    fix: [QIP-12737] 主線才要顯示團隊頁面

--- src/pages/manage/index.vue
+++ src/pages/manage/index.vue
@@ -13,7 +13,7 @@ const rootRouteLs = ['manageMember', 'manageAgent', 'manageTeam']
 const ls = computed<InstanceType<typeof SwitchTab>['$props']['tabs']>(() => [
   { id: 'manageMember', title: '会员管理', to: { name: 'manageMember' }},
   ...(!userStore.isSingleAgent ? [{ id: 'manageAgent', title: '代理管理', to: { name: 'manageAgent' }}] : []),
-  ...(userStore.hasTeam ? [{ id: 'manageTeam', title: '团队管理', to: { name: 'manageTeam' }}] : []),
+  ...(userStore.hasTeam && userStore.isMainLine ? [{ id: 'manageTeam', title: '团队管理', to: { name: 'manageTeam' }}] : []),
 ])
 const active = ref<InstanceType<typeof SwitchTab>['$props']['activeTab']>(ls.value.findIndex(l => l.id.toString() === route.name) ?? 0)
 
